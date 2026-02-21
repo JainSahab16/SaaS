a04464dc8a973ddcafe9e5a9e809096e683f1cee
JainSahab16 <153989515+JainSahab16@users.noreply.github.com>
2026-02-21 17:01:11 +0530

    cahnging the position of html

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -15,9 +15,9 @@ def home_page_view(request,*args,**kwargs):
       "queryset":page_qs.count(),
       "total_visit_count":qs.count(),
    }
+   html_template="home.html"
    path=request.path
    print(path)
-   html_template="home.html"
    PageVisit.objects.create(path=request.path)
    return render(request,html_template,my_context)
 
